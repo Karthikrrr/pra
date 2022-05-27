03a006e82ce695248b5566be5823382ef1bbe2d7
karthikrrr <karthikrkarthi6@gmail.com>
2022-05-27 09:47:36 +0530

    kkk

---
+++ ji.java
@@ -0,0 +1,7 @@
+class ji
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Ad");
+  }
+}
---
+++ ok.java
@@ -0,0 +1,7 @@
+class ok
+{
+  public static void main(String[] args)
+  {
+    System.out.print("Ad");
+  } 
+}
